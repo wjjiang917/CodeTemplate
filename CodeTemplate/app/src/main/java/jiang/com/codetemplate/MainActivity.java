e1c940e72197c62aed47a571205830135c594897
Jiangwenjin <jiang@thankyo.net>
2016-04-12 19:59:33 +0500

    RxAndroid

--- CodeTemplate/app/src/main/java/jiang/com/codetemplate/MainActivity.java
+++ CodeTemplate/app/src/main/java/jiang/com/codetemplate/MainActivity.java
@@ -2,9 +2,8 @@ package jiang.com.codetemplate;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,14 +11,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.main_content)
+    TextView mainContent;
+
+    @Bind(R.id.button)
+    Button button;
+
+    private Subscriber<String> subscriber;
+    private Observable<String> observable;
+    private Action1<String> action1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +63,53 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //        subscriber = new Subscriber<String>() {
+//            @Override
+//            public void onCompleted() {
+//                Logger.i("onCompleted");
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Logger.e("", e);
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                Logger.i("onNext: " + s);
+//                mainContent.setText(s);
+//            }
+//        };
+
+        action1 = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                mainContent.setText(s);
+            }
+        };
+    }
+
+    @OnClick(R.id.button)
+    public void clickButton(View view) {
+//        observable = Observable.create(new Observable.OnSubscribe<String>() {
+//            @Override
+//            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("Hello RxAndroid~");
+//                subscriber.onCompleted();
+//            }
+//        });
+
+//        observable = Observable.just("Hello RxAndroid~");
+
+        Observable.just("Hello RxAndroid~").map(new Func1<String, String>() {
+            @Override
+            public String call(String s) {
+                return s + " by jiang";
+            }
+        }).subscribe(action1);
+
+//        observable.subscribe(subscriber);
     }
 
     @Override
